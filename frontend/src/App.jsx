e3fb671c108a665d42c657db9e124b90bc98d17f
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-07-22 13:35:16 -0400

    add BASE_URL from Render.com in App.jsx fetch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from 'react'
 
 export default function App() {
+  const BASE_URL =  (process.env.NODE_ENV == 'production') ?
+                    'https://mern-olea.onrender.com' : 
+                    'http://localhost:1435'
   const [dinnerItems, setDinnerItems] = useState([])
   
   const getDinnerItems = ()=>{
-    fetch('/api/dinner')
+    fetch(`${BASE_URL}/api/dinner`)
       .then(res=>res.json())
       .then(json=>setDinnerItems(json))
       .catch(err=>console.log(err))
