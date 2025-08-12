2cf295339d21804a7ee7cd30d2ba47ec72cdbcb7
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-08-12 15:54:40 -0400

    add link to open table in OpenTable.jsx

--- frontend/src/components/OpenTable.jsx
+++ frontend/src/components/OpenTable.jsx
@@ -6,7 +6,10 @@ export default function OpenTable(){
                 <div className='open-table'>
                   <div id='ot1'>make a reservation</div>
                   <div id='ot2'>POWERED BY OPENTABLE</div><br/>
-                  <span id='ot3'>FIND A TABLE</span>
+                  <a  href='https://www.opentable.com/rest_profile.aspx?rid=151186&restref=151186'
+                      target='_blank'>
+                    <span id='ot3' style={{cursor:'pointer'}}>FIND A TABLE</span>
+                  </a>
                 </div>{/* #open-table */}
 
         </>
