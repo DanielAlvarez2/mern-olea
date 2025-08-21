c11d1985511be2257696a74e141f9e59a0d5eb6b
Daniel Alvarez <daniel.yllanes@hotmail.com>
2025-08-20 23:48:39 -0400

    in ManagerPageForm.jsx add input fields

--- frontend/src/components/ManagerPageForm.jsx
+++ frontend/src/components/ManagerPageForm.jsx
@@ -9,13 +9,21 @@ export default function ManagerPageForm(){
     }
 
     function editDinnerItem(id){
-        
+
     }
     return(
         <>
             <h1>Manager Page Form</h1>
             <form action={editForm ? editDinnerItem : addDinnerItem}>
                 <h2>{editForm ? 'Edit' : 'Create New'} Item</h2>
+
+                    Section:<br/>
+                <select>
+                    <option disabled>Select Section...</option>
+                    <option>Meats</option>
+                    <option>Appetizers</option>
+                    <option>Entrées</option>
+                </select>
             </form>
         </>
     )
